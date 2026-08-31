006a727fc6e850849f2b4c4b15f75b7ba805ead4
skeppybob <wilkeskbarrett@gmail.com>
2026-08-31 12:15:26 -0400

    Update alias path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
